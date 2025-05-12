6a93b34570790a561c73050e269dfc730f81f556
Triickstr <jvcleite@gmail.com>
2025-05-12 10:21:49 -0400

    index.js

--- index.js
+++ index.js
@@ -2,5 +2,15 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.js';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+const rootElement = document.getElementById('root');
+
+if (rootElement) {
+  const root = ReactDOM.createRoot(rootElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+} else {
+  console.error("Root element not found. Make sure your index.html has a <div id='root'></div>");
+}
